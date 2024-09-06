95254c03849da31fc4ddd56cb2c9646181871fcd
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-09-06 16:24:57 +0200

    Core finished

---
+++ db/migrations/V1_1_0__insert_dummy_data_to_emplyee.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Employee (name, job_name, salary_grade, department)
+VALUES
+('Tommy', 'Software Engineer', 'B', 'IT'),
+('Sarah', 'Data Analyst', 'A', 'IT'),
+('John', 'Product Manager', 'C', 'Marketing'),
+('Emma', 'HR Specialist', 'B', 'Human Resources'),
+('David', 'Network Administrator', 'B', 'IT'),
+('Lisa', 'Graphic Designer', 'A', 'Marketing'),
+('Michael', 'Business Analyst', 'C', 'Finance'),
+('Sophia', 'Content Writer', 'A', 'Marketing'),
+('James', 'DevOps Engineer', 'B', 'IT'),
+('Olivia', 'Sales Executive', 'C', 'Sales');
